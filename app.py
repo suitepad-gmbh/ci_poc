a9dea8616c3342088959556e2f1442755300be88
Rajat Gupta <rajat.gupta712@gmail.com>
2017-07-18 11:46:58 +0200

    Added info

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def hello_world():
     return 'This is home page.'
 
+@app.route('/info')
+def info():
+    return 'This is info page.'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
